945458d62ad3a63064aa13c5dd67d76fec63d7c6
Estarlin Lopez <45240343+ELopez2018@users.noreply.github.com>
2025-05-07 15:11:35 -0500

    services

--- src/app/pages/dashboard/Service/loans/loans-api.service.ts
+++ src/app/pages/dashboard/Service/loans/loans-api.service.ts
@@ -15,4 +15,40 @@ export class LoansApiService {
     return this.http
       .post<any>(url, loan)
   }
+
+  public approveLoan$(loanId: number): Observable<any> {
+    const url = `${this.server}/api/loans/approve/${loanId}`
+    return this.http
+      .post<any>(url, {} )
+  }
+
+  public rejectLoan$(loanId: number): Observable<any> {
+    const url = `${this.server}/api/loans//reject/${loanId}`
+    return this.http
+      .post<any>(url, {})
+  }
+
+  public getLoanByStatus$(loan: Loan): Observable<any> {
+    const url = `${this.server}/api/loans/apply`
+    return this.http
+      .post<any>(url, loan)
+  }
+
+  public getLoanById$(loan: Loan): Observable<any> {
+    const url = `${this.server}/api/loans/apply`
+    return this.http
+      .post<any>(url, loan)
+  }
+
+  public getLoansByUser$(loan: Loan): Observable<any> {
+    const url = `${this.server}/api/loans/apply`
+    return this.http
+      .post<any>(url, loan)
+  }
+
+  public updateLoanStatus$(loan: Loan): Observable<any> {
+    const url = `${this.server}/api/loans/apply`
+    return this.http
+      .post<any>(url, loan)
+  }
 }
